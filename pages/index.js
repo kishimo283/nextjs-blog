3ae35877998804214730abadce91123d6f7f8d11
Tsubasa Kishimoto <tsuba-soccer283@outlook.jp>
2021-06-02 12:37:54 +0900

    add text

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>ぼくは翼です。好きな食べ物はカレーです。運動大好きです。</p>
+        <p>ぼくは翼です。好きな食べ物はカレーです。運動大好きです。週末フットサル。</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
